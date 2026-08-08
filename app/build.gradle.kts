49b8415545d797da11884ca04a1454b6821a150b
256x <admin@fumi.day>
2026-08-09 07:31:41 +0900

    Bump version to 1.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "fumi.day.literalmemo"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.3.0"
+        versionCode = 21
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
