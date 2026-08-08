f95eb68811bc8c6e90165930424d43a3044b858d
256x <admin@fumi.day>
2026-08-09 07:55:50 +0900

    Bump version to 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 21
-        versionName = "1.3.1"
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
